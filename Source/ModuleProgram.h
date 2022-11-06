0538c3c688571747a28d54e9621cad6a85c8f120
TheGeminis <janmarc.costa01@estudiant.upf.edu>
2022-11-06 19:39:46 +0100

    added squeleton methods for 2 new modules

--- Source/ModuleProgram.h
+++ Source/ModuleProgram.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Module.h"
+
+class ModuleProgram : public Module 
+{
+
+public:
+	ModuleProgram();
+	~ModuleProgram();
+
+	bool Init();
+	update_status PreUpdate();
+	update_status Update();
+	update_status PostUpdate();
+	bool CleanUp();
+};
---
+++ Source/ModuleRenderExercise.cpp
@@ -0,0 +1,30 @@
+#include "ModuleRenderExercise.h"
+
+ModuleRenderExercise::ModuleRenderExercise()
+{
+}
+
+// Destructor
+ModuleRenderExercise::~ModuleRenderExercise()
+{
+}
+
+bool ModuleRenderExercise::Init()
+{
+}
+
+update_status ModuleRenderExercise::PreUpdate()
+{
+}
+
+update_status ModuleRenderExercise::Update()
+{
+}
+
+update_status ModuleRenderExercise::PostUpdate()
+{
+}
+
+bool ModuleRenderExercise::CleanUp()
+{
+}
